001b10d40753cc1d1ad4fb474ae839166b6e6a89
Sachin Jha <Sachin.Jha@walmart.com>
2017-06-12 15:14:59 -0500

    Simple observer arrow functions

--- main.js
+++ main.js
@@ -4,22 +4,10 @@ let numbers = [1,2,3,5,7];
 //Create Observable of stream of numbers
 let source = Observable.from(numbers);
 
-class MyObserver {
-    next(value){
-        console.log(`Value is ${value}`);
-    }
-
-    error(e){
-        console.log(`Error is ${e}`);
-    }
-
-    complete(){
-        console.log(`Observer is completed!!!`)
-    }
-}
 
 
-//Subscribe to observable. Pass observer to handle streams of data coming from Observable
-source.subscribe(new MyObserver());
-//You can have multiple observers for a single observable
-source.subscribe(new MyObserver());
+source.subscribe(
+    value => console.log(`Value is ${value}`),
+    e => console.log(`Error is ${e}`),
+    () => console.log(`Observer is completed!!!`)
+);
